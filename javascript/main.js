074c0cb2940d956e5d032bacb40b3c2bdc9ef963
Oluchukwu Divine <oluchukwudivine31@gmail.com>
2024-05-22 13:54:38 +0100

    dropdown edit

---
+++ javascript/main.js
@@ -0,0 +1,21 @@
+const toggleDisplay = document.querySelector(".toggle-left-drop-down");
+const leftOverflow = document.querySelector(".left-overflow");
+const changeInboxMsg = document.querySelector(".change-inbox-msg");
+
+let toggle = true;
+
+toggleDisplay.addEventListener("click", () => {
+  if (toggle == true) {
+    leftOverflow.style.height = "70%";
+    leftOverflow.style.overflowY = "scroll";
+    toggle = false;
+    changeInboxMsg.textContent="Less";
+  }
+
+  else if(toggle == false){
+    leftOverflow.style.height = "225px";
+    leftOverflow.style.overflowY = "hidden";
+    toggle = true;
+    changeInboxMsg.textContent="more";
+  }
+});
